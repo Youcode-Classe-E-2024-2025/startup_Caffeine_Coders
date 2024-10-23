db01ed02d6cd27d242f81db69f7ef6e94ae5fed8
echchabli hamza <echchablihamza1@gmail.com>
2024-10-23 22:37:25 +0100

    detailsTransfer

--- code/js/script.js
+++ code/js/script.js
@@ -65,3 +65,46 @@ function fetchProducts() {
 //  }
 
 
+let list = []; 
+const img = document.querySelector('.productImg');
+const nname = document.querySelector('.productName');
+const stock = document.querySelector('.stock-status');
+const desc = document.querySelector('.desc');
+const price = document.querySelector('.price');
+
+
+window.onload = function() {
+    fetchProducts(); 
+};
+
+function fetchProducts() {
+    fetch('../js/data.json') 
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok: ' + response.statusText);
+            }
+            return response.json(); 
+        })
+        .then(products => {
+            list = products;
+            displayProductDetails();
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+        });
+}
+
+function displayProductDetails() {
+    const selectedProductId = localStorage.getItem('selectedProductId'); 
+    const product = list.find(item => item.id == selectedProductId); 
+
+    if (product) {
+        img.setAttribute('src', product.img);
+        nname.textContent = product.name;
+        stock.textContent = '40 in stock'; 
+        price.textContent = `${product.price} DH`;
+        desc.textContent =  product.name; 
+    } else {
+        console.error(`Product with ID ${selectedProductId} not found.`);
+    }
+}
